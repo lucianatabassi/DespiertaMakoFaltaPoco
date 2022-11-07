1d985846635d99f10c62ac364305218051dde610
lucianatabassi <lutabassi@gmail.com>
2022-11-07 17:16:42 -0300

    Initial commit

---
+++ Assets/Scripts/ScriptsN3/PlayVideo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+public GameObject videoPlayer;
+public int timeToStop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+void OnTriggerEnter2D(Collider2D player){
+
+    if(player.gameObject.tag=="Player")
+    {
+        AudioSource[] audios = FindObjectsOfType<AudioSource>();
+        foreach(AudioSource a in audios)
+        {
+            a.Pause();
+        }
+        
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer,timeToStop);
+}
+}
+}
---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class bullet : MonoBehaviour
+{
+   // private Rigidbody2D MyRB;
+    public float speed;
+    public float hit = 1;
+
+ 
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+        transform.position += transform.right * speed * Time.deltaTime; //transforma y varia la posicion el objeto
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        var enemy = collision.collider.GetComponent<enemigo>(); // el script enemigo se enlaza con este
+        var enemigo1 = collision.collider.GetComponent<Enemigo1>();
+        var enemigo3 = collision.collider.GetComponent<MovEnemigoPlataforma>();
+        var boss = collision.collider.GetComponent<Boss>();
+        
+        if (enemy) {
+            enemy.TakeHit(hit); //nivel de daño que le pongamos
+            Destroy(gameObject);
+        }
+
+        if (enemigo1) {
+            enemigo1.TakeHit(hit); //Hacer x puntos de daño
+            Destroy(gameObject);
+        }
+        if (enemigo3) {
+            enemigo3.TakeHit(hit); //Hacer x puntos de daño
+            Destroy(gameObject);
+        }
+        if (boss) {
+            boss.TakeHit(hit); //Hacer x puntos de daño
+            Destroy(gameObject);
+        }
+        if (collision.transform.tag =="ground" ) {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+}
